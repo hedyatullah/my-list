b40e4aeed38dd77493121d88d591c4184ceba304
hedyatullah <43230105+hedyatullah@users.noreply.github.com>
2018-10-09 15:42:44 -0400

    update

--- src/ListItem.js
+++ src/ListItem.js
@@ -4,15 +4,28 @@ import List from './List.js';
 import style from './style.css';
 
 
-    const ListItem = ( { item } ) => {
-    //console.log(this.props.deleteuser);
+    const ListItem = (props) => {
+    //console.log(this.props.deleteuser);        
     return(
         <div>
-            <h3>{item.name} : {item.job} 
-                <a onClick={deleteEvent.item.id} className="delete" href="#">x</a> </h3>
+            <h3>{props.item.name} : {props.item.job} 
+                <a onClick={() => 
+                    {props.deleteEvent(props.item.id)}
+            } className="delete" href="#">x</a> </h3>
             <p>{item.description}</p>
         </div>
     )
 }
+    
+ //   const ListItem = ( { item } ) => {
+    //console.log(this.props.deleteuser);        
+  //  return(
+   //     <div>
+    //        <h3>{item.name} : {item.job} 
+     //           <a onClick={deleteEvent.item.id} className="delete" href="#">x</a> </h3>
+      //      <p>{item.description}</p>
+      //  </div>
+   // )
+// }
 
 export default ListItem;
